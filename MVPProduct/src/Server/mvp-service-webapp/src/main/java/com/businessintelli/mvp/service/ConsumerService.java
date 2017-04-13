afa1a611b7def8947681cc530dd6891c203803f0
Rama Krishna <ramakrishna.candy@gmail.com>
2017-04-13 14:03:40 -0400

    changes

--- MVPProduct/src/Server/mvp-service-webapp/src/main/java/com/businessintelli/mvp/service/ConsumerService.java
+++ MVPProduct/src/Server/mvp-service-webapp/src/main/java/com/businessintelli/mvp/service/ConsumerService.java
@@ -6,6 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 //import java.util.Map;
 //import java.util.Scanner;
+import java.util.HashMap;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
@@ -34,13 +35,39 @@ public class ConsumerService
 	
 	public int create(Consumer consumer) throws Exception
 	{
-		logger.info("Adding consumer");
+		/*logger.info("Adding consumer");
 		String consumerStr = consumer.toString();
 		//fw.write(consumerStr);
 		fw.append(consumerStr);
 		fw.flush();
 		logger.info("consumer added");
-		return consumer.getId();
+		return consIntegertId();*/
+		Consumer cs1 = new Consumer();
+		cs1.setId(1);
+		cs1.setfirstName("ram");
+		cs1.setEmail("ram@gmail.com");
+		cs1.setlastName("kandagatla");
+		cs1.setPhone("+8148568955");
+		
+		
+		Consumer cs2 = new Consumer();
+		cs2.setId(2);
+		cs2.setfirstName("ram");
+		cs2.setEmail("ram@gmail.com");
+		cs2.setlastName("kandagatla");
+		cs2.setPhone("+8148568955");
+		
+		Consumer cs3 = new Consumer();
+		cs3.setId(3);
+		cs3.setfirstName("ram");
+		cs3.setEmail("ram@gmail.com");
+		cs3.setlastName("kandagatla");
+		cs3.setPhone("+8148568955");
+		
+		HashMap<integer, String> HMap = new HashMap<integer, String>;
+		HMap.put(1,"cs1");
+		HMap.put(1,"cs2")
+		HMap.put(1,"cs3")
 	}
 
 	public Consumer retrive(int pId) 
--- MVPProduct/src/UI/Views/MarketerProducts/ReadMe.txt
+++
@@ -1,1 +0,0 @@
-package MVPPrototype.Components
